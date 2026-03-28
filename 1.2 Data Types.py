b0cf35e892986f959fb585d1f54c03390ec36f41
deshpanderaza305@gmail.com <MohdRaza-Firoj-Deshpande>
2026-03-28 19:39:04 +0530

    Data Types

---
+++ 1.2 Data Types.py
@@ -0,0 +1,58 @@
+'''
+DataTypes
+1. Definition:
+Data types are a classification of data which tell the compiler or interpreter how the programmer intends to use the data.
+They determine the type of operations that can be performed on the data, the values that the data can take, and the amount of memory needed to store the data.
+2. Importance of Data Types in Programming
+Explanation:
+
+Data types ensure that data is stored in an efficient way.
+They help in performing correct operations on data.
+Proper use of data types can prevent errors and bugs in the program.
+
+
+Introduction to Data Types
+Importance of Data Types in Programming
+Basic Data Types
+Integers
+Floating-point numbers
+Strings
+Booleans
+Advanced Data Types
+Lists
+Tuples
+Sets
+Dictionaries
+Type Conversion
+Practical Examples
+
+'''
+
+
+## Integer Example
+
+age=35
+print(type(age))
+
+##floating point datatype
+height=5.11
+print(height)
+print(type(height))
+
+## string datatype example
+name="Krish"
+print(name)
+print(type(name))
+
+## boolean datatype
+is_true=True
+type(is_true)
+
+a=10
+b=10
+
+type(a==b)
+result="Hello" + str(5)
+print(result)
+
+print(type(result))
