ab1072838a6e4037131188d814aa24d64a2ff872
Mohd.Raza Firoj Deshpande <61143716+MohdRaza-Firoj-Deshpande@users.noreply.github.com>
2026-03-31 21:44:12 +0530

    Add inventory management functionality

--- 3. Data Structures/3.2 list_example.py
+++ 3. Data Structures/3.2 list_example.py
@@ -53,6 +53,27 @@ print(f"Higest Grade : {higest_grade}")
 print(f"Lowest Grade :{lowest_grade}")
 
 
+# Managing an inventory
+inventory = ["apples", "bananas", "oranges", "grapes"]
+
+# Adding a new item
+inventory.append("strawberries")
+
+# Removing an item that is out of stock
+inventory.remove("bananas")
+
+# Checking if an item is in stock
+item = "oranges"
+if item in inventory:
+    print(f"{item} are in stock.")
+else:
+    print(f"{item} are out of stock.")
+
+# Printing the inventory
+print("Inventory List:")
+for item in inventory:
+    print(f"- {item}")
+
 
 
 #Collecting user feedback
