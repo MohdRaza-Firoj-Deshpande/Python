b11fd914c19e0903b53b1b44e0c06e7e29fbc559
deshpanderaza305@gmail.com <MohdRaza-Firoj-Deshpande>
2026-03-31 08:21:03 +0530

    list example

---
+++ 3. Data Structures/3.2 list_example.py
@@ -0,0 +1,53 @@
+"""
+Real-World Examples Using Lists in Python
+Lists are one of the most commonly used data structures in Python, thanks to their versatility and ease of use. 
+Here are several practical examples that illustrate their use in real-world scenarios
+
+"""
+
+"Example 1. Manage A To Do List"
+
+"Create a To Do List To Keep Track OF Tasks"
+
+
+
+to_do_list=["Buy Groceries","Clean the house","Pay bills"]
+
+## Adding to task
+to_do_list.append("Schedule meeting")
+to_do_list.append( "Go For a Run")
+
+## Removing a completed task
+to_do_list.remove("Clean the house")
+
+## checking if a task is in the list
+if "Pay bills" in to_do_list:
+    print("Don't forgrt to pay the utility bills")
+
+print("To Do List remaining")
+for task in to_do_list:
+    print(f"{task}") 
+
+
+"""
+Example 2: Organizing Student Grades
+Create a list to store and calculate average grades for students
+
+"""  
+
+# Organizing student grades
+grades = [85, 92, 78, 90, 88]
+
+# Adding a new grade
+grades.append(95)
+
+
+# Calculating the average grade
+average_grade = sum(grades) / len(grades)
+print(f"{average_grade:.2f}")
+
+higest_grade= max(grades)
+lowest_grade = min(grades)
+
+print(f"Higest Grade : {higest_grade}")
+print(f"Lowest Grade :{lowest_grade}")
