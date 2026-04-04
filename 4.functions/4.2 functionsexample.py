839b9d32585912f8d874845bfa96231049e09497
deshpanderaza305@gmail.com <MohdRaza-Firoj-Deshpande>
2026-04-04 21:58:07 +0530

    functionsexample1

--- 4.functions/4.2 functionsexample.py
+++ 4.functions/4.2 functionsexample.py
@@ -1,5 +1,5 @@
 
-"""
+"""w
 Functions Examples
 Example 1: Temperature Conversion
 """
@@ -24,7 +24,7 @@ Example 2: Password Strength Checker
 def password_strenght_cheker(password):
     if len(password)<8:
         return False
-    if not any (char.islower() for char in password):
+    if not any(char.islower() for char in password):
         return False
     if not any(char.isupper() for char in password):
         return False
@@ -36,3 +36,65 @@ def password_strenght_cheker(password):
     
 print(password_strenght_cheker("1235678u")  )  
 print(password_strenght_cheker("1235678uE@")  )  
+
+
+def panlindrome(s):
+    s=s.lower().replace(" ","")
+    if s==s[::-1]:
+        return True
+
+
+print(panlindrome("A man a plan a canal Panama"))
+
+
+#factorial( 6! = 1 *2 *3 *4 *5 *6)
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+         return n* factorial(n-1)
+
+print(factorial(6))
+
+import os
+
+os.chdir(r'4.functions')  # paste your folder path here
+
+print(os.getcwd())  # confirm it changed
+
+print("-->")
+
+def word_counter(filename):
+    counter={}
+    with open(filename,'r') as file:
+        for line in file:
+            print (line)
+        words=line.split()   
+        print(words)
+        for word in words:
+            word=word.lower().strip('!@#$%^&*')
+            print(word)
+            counter[word]= counter.get(word,0)+1
+    return counter
+
+
+filename='main.txt'
+con =  word_counter(filename)
+print(con)
+
+
+def local_counter(name):
+    counts={}
+
+    b= name.split()
+    for e in b:
+        e=e.lower().strip('!@#$%^')
+        print(f"{e},e working")
+        counts[e] = counts.get(e,0)+1
+
+    return counts
+name ='Aeiou olkjo@#$'
+com=  local_counter(name)
+print(com)
+
